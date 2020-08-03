c9a6aad8881d3210df15554c0be1f0c4f0f9dea3
Rafael Carrillo <masterxp79@gmail.com>
2020-08-02 20:02:26 -0700

    temporaly remove profile pic

--- src/components/Sidebar/Profile/Profile.js
+++ src/components/Sidebar/Profile/Profile.js
@@ -4,7 +4,7 @@ import profilePicture from '../../../images/developer.png'
 function Profile(props) {
     return (
         <div className="profile-container">
-            <img src={profilePicture} className="profile" alt="logo" />
+            {/* <img src={profilePicture} className="profile" alt="logo" /> */}
             <h1 className="name">{props.name}</h1>
             <h3 className="tagline">{props.tagline}</h3>
         </div>
